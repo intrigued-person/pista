b0f496b45359f474c03b5372870a8eb73620941c
intrigued-person <122249915+intrigued-person@users.noreply.github.com>
2024-07-03 10:27:26 +0530

    Add files via upload
    pista buroooo

---
+++ Pista/src/main/java/com/springmvc/HomeController.java
@@ -0,0 +1,18 @@
+package com.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping("pasta")
+	public String pasta() {
+		return "pasta";
+	}
+}
